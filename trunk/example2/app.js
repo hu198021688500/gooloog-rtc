8e5bc8d7aaa2fcb93c6e7833996691f46fad2e09
huguobing2012 <huguobing2012@93895ce9-919c-a2d4-0779-7347b392e663>
2012-12-28 02:34:29 +0000

    初始导入。

--- trunk/example2/app.js
+++ trunk/example2/app.js
@@ -1,5 +1,11 @@
 module.exports.conf = require('./config/config');
-var http = require('http'), rrest = require('rrestjs');
-http.createServer(function(req, res) {
-	res.send('hello world everyone!');
-}).listen(rrest.config.listenPort);
+
+var http = require('http');
+var rrest = require('rrestjs');
+var app = require('./controller/app.js');
+
+var server = http.createServer(rrest(function(req, res) {
+	app(req, res);
+})).listen(rrest.config.listenPort);
+
+_rrest = rrest;
